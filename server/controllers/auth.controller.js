a1bd973b3127a041ac04f55a541f3e445d89dbd4
Piotr Jakub Dębski <piotrdb1@gmail.com>
2022-07-05 15:12:58 +0200

    controllers added

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,94 @@
+const db = require("../models");
+const config = require("../config/auth.config");
+const User = db.user;
+const Role = db.role;
+const Op = db.Sequelize.Op;
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+exports.register = async (req, res) => {
+  try {
+    const user = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 8),
+    });
+
+    if (req.body.roles) {
+      const roles = await Role.findAll({
+        where: {
+          name: {
+            [Op.or]: req.body.roles,
+          },
+        },
+      });
+
+      const result = user.setRoles(roles);
+
+      if (result) res.send({ message: "User registered successfully!" });
+    } else {
+      const result = user.setRoles([1]);
+      if (result) res.send({ message: "User registered successfully!" });
+    }
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        username: req.body.username,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).send({ message: "User not found." });
+    }
+
+    const passwordIsValid = bcrypt.compareSync(
+      req.body.password,
+      user.password
+    );
+
+    if (!passwordIsValid) {
+      return res.status(401).send({
+        message: "Invalid Password!",
+      });
+    }
+
+    const token = jwt.sign({ id: user.id }, config.secret, {
+      expiresIn: 86400,
+    });
+
+    let authorities = [];
+    const roles = await user.getRoles();
+
+    for (let i = 0; i < roles.length; i++) {
+      authorities.push("ROLE_" + roles[i].name.toUpperCase());
+    }
+
+    req.session.token = token;
+
+    return res.status(200).send({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      roles: authorities,
+    });
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+};
+
+exports.logout = async (req, res) => {
+  try {
+    req.session = null;
+    return res.status(200).send({
+      message: "You've been logged out!",
+    });
+  } catch (err) {
+    this.next(err);
+  }
+};
