7e1ede2453d05cd94f0179cc498263a423d86c8a
Piotr Jakub Dębski <piotrdb1@gmail.com>
2022-07-05 15:13:27 +0200

    routes added

---
+++ server/routes/auth.routes.js
@@ -0,0 +1,24 @@
+const { verifyRegister } = require("../middleware");
+const controller = require("../controllers/auth.controller");
+const verifyRegister = require("../middleware/verifyRegister");
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.post(
+    "/api/auth/register",
+    [
+      verifyRegister.checkDuplicateUsernameOrEmail,
+      verifyRegister.checkRolesExisted
+    ],
+    controller.register
+  );
+  app.post("/api/auth/login", controller.login);
+  app.post("/api/auth/logout", controller.logout);
+};
